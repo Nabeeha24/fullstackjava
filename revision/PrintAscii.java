87cd1e413344f2eac45dd82c5c353883faac11ec
Nabeeha24 <87181744+Nabeeha24@users.noreply.github.com>
2021-07-09 16:07:46 +0530

    Add files via upload

---
+++ revision/PrintAscii.java
@@ -0,0 +1,16 @@
+package revision;
+
+public class PrintAscii {
+
+	public static void main(String[] args) {
+		char ch ='H';
+		int ascii =ch;
+		
+		int castAscii = (int) ch;
+		
+		System.out.println("The ASCII value of" + ch + "is"  + ascii);
+		System.out.println("The ASCII value of" + ch + "is" + castAscii);
+		
+	}
+
+}
